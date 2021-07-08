a314bbed671f724d0a1d0fc463ecf15f71d9d0b2
chuckskull1 <dimrirohit2@gmail.com>
2021-07-08 21:50:06 +0530

    Update Program.cs

--- Devops/Program.cs
+++ Devops/Program.cs
@@ -20,7 +20,7 @@ namespace Devops
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>()
+                    webBuilder.UseStartup<Startup>();
                 });
     }
 }
